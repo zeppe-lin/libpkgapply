e44acbae68366224e0c5af8151146ceee3488a6d
Alexandr Savca <alexandr.savca89@gmail.com>
2026-07-24 13:24:17 +0000

    posix: stage private incoming payloads

--- include/libpkgapply-posix/libpkgapply-posix.h
+++ include/libpkgapply-posix/libpkgapply-posix.h
@@ -5,4 +5,5 @@
 
 #include <libpkgapply-posix/checkpoint_store.h>
 #include <libpkgapply-posix/journal_store.h>
+#include <libpkgapply-posix/payload_stage.h>
 #include <libpkgapply-posix/target_observer.h>
